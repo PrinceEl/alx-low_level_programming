6220e7e825978c54dda1f1e030c065050b488774
PrinceEl <oluwole.kasali@gmail.com>
2023-06-15 21:53:37 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Short Description, Single Line
+ * Description: Longer Description of the Function
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+ALX Software Engineering "C" - Hello World Program, An Introduction To C Language
