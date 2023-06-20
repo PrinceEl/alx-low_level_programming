0fb2250f40787d7ae88a8c703c018e7a89bbc684
PrinceEl <oluwole.kasali@gmail.com>
2023-06-20 16:28:36 -0700

    Function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet x10
+ * Return: 0(Success)
+ */
+void print_alphabet_x10(void)
+{
+int c2;
+int c;
+for (c2 = 0; c2 <= 9; c2++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+	}
+	_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - Check for lowercase character
+ *@c: letter being tested
+ * Return: 0(Succcess)
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
